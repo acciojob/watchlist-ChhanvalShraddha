eb13a474f200dd1cb78a2ce5a4030cd73ab1854e
ChhanvalShraddha <shraddha101020@gmail.com>
2023-01-27 15:25:39 +0530

    watchlist1

--- src/main/java/com/driver/Application.java
+++ src/main/java/com/driver/Application.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
-
+public class Application{
+	//this class intiative thst is run while spring boot project;
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
